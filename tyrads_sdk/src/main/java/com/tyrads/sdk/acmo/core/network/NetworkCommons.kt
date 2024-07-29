9db4c50511815ec0be38c875d82ee7445aaa04f0
Acmo Dev <dev@acmo.in>
2024-07-29 09:05:13 +0530

    Used new versioning format in sending the requests to server

--- tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/core/network/NetworkCommons.kt
+++ tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/core/network/NetworkCommons.kt
@@ -37,14 +37,19 @@ class NetworkCommons() {
                     request.header("X-SDK-Platform", "Android")
                     request.header("X-SDK-Version", "v0.1.0")
                     request.header("Content-Type", "application/json")
-                    val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(Tyrads.getInstance().context!!)
+                    val sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(Tyrads.getInstance().context!!)
 
                     if (!request.url.path.endsWith(AcmoEndpointNames.INITIALIZE)) {
-                        request.headers["X-User-ID"] = sharedPreferences.getString(AcmoKeyNames.USER_ID, null) ?: ""
+                        request.headers["X-User-ID"] =
+                            sharedPreferences.getString(AcmoKeyNames.USER_ID, null) ?: ""
                     }
-                    request.headers["X-API-Key"] = sharedPreferences.getString(AcmoKeyNames.API_KEY, null) ?: ""
-                    request.headers["X-API-Secret"] = sharedPreferences.getString(AcmoKeyNames.API_SECRET, null) ?: ""
-
+                    request.headers["X-API-Key"] =
+                        sharedPreferences.getString(AcmoKeyNames.API_KEY, null) ?: ""
+                    request.headers["X-API-Secret"] =
+                        sharedPreferences.getString(AcmoKeyNames.API_SECRET, null) ?: ""
+                    request.headers["X-SDK-Platform"] = AcmoConfig.SDK_PLATFORM
+                    request.headers["X-SDK-Version"] = AcmoConfig.SDK_VERSION
                 }
                 next(request)
             }
@@ -60,7 +65,6 @@ class NetworkCommons() {
             }
         }
     }
-
 
 
 }
