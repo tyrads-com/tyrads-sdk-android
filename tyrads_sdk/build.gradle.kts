3978402a6ea95801e302647c17d5c8cceb80d9e9
Acmo Dev <dev@acmo.in>
2024-09-29 13:03:38 +0530

    downgraded gradle version to 7.4.2

--- tyrads_sdk/build.gradle.kts
+++ tyrads_sdk/build.gradle.kts
@@ -52,11 +52,11 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+//    packaging {
+//        resources {
+//            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+//        }
+//    }
 }
 
 dependencies {
