37174f95c94b7e6a027a8bf008c3a9ba7f6b6297
basharatmehdi <basharatmehdidar@gmail.com>
2025-06-27 16:50:11 +0530

    fixed gradle and compilesdk issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     defaultConfig {
         applicationId = "com.tyrads.sdk.example"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
