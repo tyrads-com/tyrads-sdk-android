7ffac95a4f16ad6d4f14f2b68bc6788ca1839e18
basharatmehdi <basharatmehdidar@gmail.com>
2025-07-04 12:39:17 +0530

    other changes

---
+++ example_java/build/generated/source/buildConfig/debug/com/example/example_java/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.example_java;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.example_java";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final String TYRADS_API_KEY = "";
+  // Field from build type: debug
+  public static final String TYRADS_API_SECRET = "";
+}
