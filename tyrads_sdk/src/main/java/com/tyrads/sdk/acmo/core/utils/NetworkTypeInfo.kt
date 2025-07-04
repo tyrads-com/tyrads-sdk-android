c081f964cf595970579f59fadd41a5f1b58ae0b6
basharatmehdi <basharatmehdidar@gmail.com>
2025-07-04 12:09:43 +0530

    method created for network type info

---
+++ tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/core/utils/NetworkTypeInfo.kt
@@ -0,0 +1,45 @@
+package com.tyrads.sdk.acmo.core.utils
+
+import android.Manifest
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.telephony.TelephonyManager
+import androidx.annotation.RequiresPermission
+
+@RequiresPermission(anyOf = [Manifest.permission.READ_BASIC_PHONE_STATE, Manifest.permission.READ_PHONE_STATE])
+fun getNetworkType(context: Context): String {
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+    val network = connectivityManager.activeNetwork ?: return "No Connection"
+    val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return "Unknown"
+
+    return when {
+        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> "WiFi"
+        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> "Ethernet"
+        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+            val networkType =
+                telephonyManager.dataNetworkType
+
+            when (networkType) {
+                TelephonyManager.NETWORK_TYPE_NR -> "Mobile - 5G"
+                TelephonyManager.NETWORK_TYPE_LTE -> "Mobile - 4G LTE"
+                TelephonyManager.NETWORK_TYPE_HSPAP,
+                TelephonyManager.NETWORK_TYPE_UMTS,
+                TelephonyManager.NETWORK_TYPE_EVDO_0,
+                TelephonyManager.NETWORK_TYPE_EVDO_A,
+                TelephonyManager.NETWORK_TYPE_EVDO_B -> "Mobile - 3G"
+                TelephonyManager.NETWORK_TYPE_EDGE,
+                TelephonyManager.NETWORK_TYPE_GPRS,
+                TelephonyManager.NETWORK_TYPE_CDMA,
+                TelephonyManager.NETWORK_TYPE_1xRTT -> "Mobile - 2G"
+                TelephonyManager.NETWORK_TYPE_UNKNOWN -> "Mobile - Unknown Type"
+                else -> "Mobile Data"
+            }
+        }
+        else -> "Unknown"
+    }
+}
+
+
