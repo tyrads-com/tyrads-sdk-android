b5e5e30f14f1aa06aaa5b4abc561363da7bb48bf
basharatmehdi <basharatmehdidar@gmail.com>
2025-07-07 12:51:10 +0530

    added encryption for api post reqs

--- tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/modules/usage_stats/Repository.kt
+++ tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/modules/usage_stats/Repository.kt
@@ -1,15 +1,20 @@
-import android.util.Log
 import androidx.annotation.Keep
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.result.Result
 import com.google.gson.Gson
+import com.tyrads.sdk.Tyrads
+import com.tyrads.sdk.acmo.helpers.AcmoEncrypt
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+
 @Keep
 class AcmoUsageStatRepository {
     suspend fun saveUsageStats(fd: Map<String, Any>): Any = withContext(Dispatchers.IO) {
-        val body = Gson().toJson(fd)
-        val (_, response, result) = Fuel.post( AcmoEndpointNames.USAGE_STATS)
+        val encKey: String =
+            Tyrads.getInstance().preferences.getString(AcmoKeyNames.ENCRYPTION_KEY, "") ?: ""
+        val encData = AcmoEncrypt(encryptionKey = encKey).encryptDataAESGCM(data = fd);
+        val body = Gson().toJson(if (Tyrads.getInstance().isSecure) encData else fd)
+        val (_, response, result) = Fuel.post(AcmoEndpointNames.USAGE_STATS)
             .body(body)
             .response()
 
