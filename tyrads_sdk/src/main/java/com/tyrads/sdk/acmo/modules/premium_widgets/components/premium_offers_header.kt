29ca23792268b35fbffb3b0845a30707a6dfde76
Naveed Bhat <naveed.bhat@acmo.in>
2025-08-29 16:11:41 +0530

    Pass localizationService from TopOffers to PremiumHeaderSection

--- tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/modules/premium_widgets/components/premium_offers_header.kt
+++ tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/modules/premium_widgets/components/premium_offers_header.kt
@@ -1,12 +1,10 @@
 package com.tyrads.sdk.acmo.modules.premium_widgets.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
@@ -17,7 +15,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -27,25 +24,23 @@ import com.tyrads.sdk.Tyrads
 import com.tyrads.sdk.acmo.core.extensions.toColor
 import com.tyrads.sdk.acmo.core.services.LocalizationService
 import com.tyrads.sdk.acmo.modules.input_models.headerIconSpacing
-import com.tyrads.sdk.acmo.modules.input_models.headerPaddingBottom
-import com.tyrads.sdk.acmo.modules.input_models.headerPaddingEnd
-import com.tyrads.sdk.acmo.modules.input_models.headerPaddingStart
-import com.tyrads.sdk.acmo.modules.input_models.headerPaddingTop
 import com.tyrads.sdk.acmo.modules.input_models.headerTextSpacing
 import kotlinx.coroutines.launch
 
 @Composable
 fun PremiumHeaderSection(
     modifier: Modifier = Modifier,
+    localizationService: LocalizationService
+
 ) {
     val coroutineScope = rememberCoroutineScope()
-    val localizationService = LocalizationService.getInstance()
 
     Row(
         modifier = modifier
             .fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
+
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
