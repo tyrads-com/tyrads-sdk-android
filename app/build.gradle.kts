4a0134770815a9fe45e28cff11db462f2a918765
basharatmehdi <basharatmehdidar@gmail.com>
2025-11-12 12:09:32 +0530

    small fix in example app and updated its version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.tyrads.sdk.example"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 4
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
