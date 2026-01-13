ddb698ac15652b9769461724ee2ab2de0126d20f
naveedbhat <naveed.bhat@acmo.in>
2026-01-13 16:05:16 +0530

    updated version code and version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.tyrads.sdk.example"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.0"
+        versionCode = 12
+        versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
