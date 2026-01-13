528a15a0deb426439a85491c4d5685d7e08d1d46
naveedbhat <naveed.bhat@acmo.in>
2026-01-13 18:56:44 +0530

    initialized FCM service in SDK initialization

--- tyrads_sdk/src/main/java/com/tyrads/sdk/Tyrads.kt
+++ tyrads_sdk/src/main/java/com/tyrads/sdk/Tyrads.kt
@@ -39,6 +39,7 @@ import com.tyrads.sdk.acmo.modules.premium_widgets.TopOffers
 import androidx.core.content.edit
 import com.tyrads.sdk.acmo.helpers.TyradsViewHelper
 import com.tyrads.sdk.acmo.modules.input_models.TyradsConfig
+import com.tyrads.sdk.acmo.modules.notifications.FCMService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -200,6 +201,13 @@ class Tyrads private constructor() {
             preferences.edit { putString(AcmoKeyNames.PLAY_INTEGRITY_TOKEN, integrityToken) }
         } catch (error: Exception) {
             log("An Error Occurred: ${error.message}", Log.ERROR)
+        }
+
+        // Initialize FCM Service
+        try {
+            FCMService.initialize(context)
+        } catch (error: Exception) {
+            log("Failed to initialize FCM: ${error.message}", Log.ERROR)
         }
 
         log("Tyrads SDK initialized", Log.INFO)
