3dc899767031c92da429aa351a342810a421192f
basharatmehdi <basharatmehdidar@gmail.com>
2026-02-21 16:18:42 +0530

    add shared urlsmatch helper function

--- tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/helpers/Commons.kt
+++ tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/helpers/Commons.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import androidx.annotation.Keep
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
+import androidx.core.net.toUri
 
 @Keep
 fun acmoLaunchURL(context: Context, url: String) {
@@ -19,4 +20,19 @@ fun isGooglePlayServicesAvailable(context: Context): Boolean {
     val googleApiAvailability = GoogleApiAvailability.getInstance()
     val resultCode = googleApiAvailability.isGooglePlayServicesAvailable(context)
     return resultCode == ConnectionResult.SUCCESS
+}
+
+@Keep
+fun urlsMatch(url1: String, url2: String): Boolean {
+    return try {
+        val uri1 = url1.toUri()
+        val uri2 = url2.toUri()
+        
+        val to1 = uri1.getQueryParameter("to") ?: ""
+        val to2 = uri2.getQueryParameter("to") ?: ""
+        
+        uri1.host == uri2.host && to1 == to2
+    } catch (e: Exception) {
+        url1 == url2
+    }
 }
