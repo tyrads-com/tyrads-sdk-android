da594b5f6bbadddcd8fc25925b92dc352f05fd9b
naveedbhat <naveed.bhat@acmo.in>
2026-06-20 11:38:25 +0530

    added user account update module with repository and controller

---
+++ tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/modules/input_models/update_user_info.kt
@@ -0,0 +1,39 @@
+package com.tyrads.sdk.acmo.modules.input_models
+
+data class TyradsUpdateUserInfo(
+    /**
+     * Can be used to identify the user to prevent fraud.
+     * Format: email address
+     */
+    val email: String? = null,
+
+    /**
+     * Can be used to identify the user to prevent fraud.
+     * Format: + should be replaced with 00, example: 0015555551234 for a US number
+     */
+    val phoneNumber: String? = null,
+
+    /** Age of the user, used for targeting. */
+    val age: Int? = null,
+
+    /**
+     * Gender of the user, used for targeting.
+     * 1 = male, 2 = female
+     */
+    val gender: Int? = null,
+
+    /** Custom field for storing additional data. */
+//    val sub1: String? = null,
+
+    /** Custom field for storing additional data. */
+    val sub2: String? = null,
+
+    /** Custom field for storing additional data. */
+    val sub3: String? = null,
+
+    /** Custom field for storing additional data. */
+    val sub4: String? = null,
+
+    /** Custom field for storing additional data. */
+//    val sub5: String? = null,
+)
---
+++ tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/modules/update_account/Controller.kt
@@ -0,0 +1,33 @@
+package com.tyrads.sdk.acmo.modules.update_account
+
+import android.util.Log
+import androidx.annotation.Keep
+import com.tyrads.sdk.Tyrads
+import com.tyrads.sdk.acmo.modules.input_models.TyradsUpdateUserInfo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+@Keep
+class AcmoUpdateUserAccountController {
+
+    private val repository = AcmoUpdateUserAccountRepository()
+
+    suspend fun updateUserAccount(userUpdateInfo: TyradsUpdateUserInfo): Boolean {
+        return withContext(Dispatchers.IO) {
+            try {
+              val isSuccess =  repository.updateUserAccount(userUpdateInfo)
+                Tyrads.getInstance().log(
+                    "AcmoUpdateUserAccountController: Account updated successfully",
+                    Log.INFO
+                )
+                isSuccess
+            } catch (e: Exception) {
+                Tyrads.getInstance().log(
+                    "AcmoUpdateUserAccountController: Failed to update account - ${e.message}",
+                    Log.ERROR
+                )
+                false
+            }
+        }
+    }
+}
