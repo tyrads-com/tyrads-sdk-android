a56c4926bfcdbdf1de8c7a31fb722c1b531c7a27
Basharat <basharat.mehdi@acmo.in>
2026-08-11 15:24:54 +0530

    Revert "Feat/localized links + example app crashlytics"

--- tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/modules/legal/PrivacyPolicy.kt
+++ tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/modules/legal/PrivacyPolicy.kt
@@ -292,9 +292,9 @@ fun Info2(localizationService: LocalizationService) {
 
                 val tag = if (matchResult.value.startsWith("<tos>")) "TOS" else "PP"
                 val annotation = if (tag == "TOS") {
-                    "https://tyrads.com/tyrsdk-terms-of-service/${Tyrads.getInstance().currentLanguageCode.value}"
+                    "https://tyrads.com/tyrsdk-terms-of-service/"
                 } else {
-                    "https://tyrads.com/tyrsdk-privacy-policy/${Tyrads.getInstance().currentLanguageCode.value}"
+                    "https://tyrads.com/tyrsdk-privacy-policy/"
                 }
                 val linkText = matchResult.groupValues[1]
 
