060cc4358fc4fc883ea1c40a1b8bf72b1419bc0e
naveedbhat <naveed.bhat@acmo.in>
2026-08-19 11:26:30 +0530

    added defaultAge and enableSkipOnboarding to TyradsConfig

--- tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/modules/input_models/TyradsConfig.kt
+++ tyrads_sdk/src/main/java/com/tyrads/sdk/acmo/modules/input_models/TyradsConfig.kt
@@ -3,4 +3,6 @@ package com.tyrads.sdk.acmo.modules.input_models
 data class TyradsConfig(
     val skipInitialPages: Boolean = false,
     val skipUserInfo: Boolean = false,
+    val defaultAge: Int? = null,
+    val enableSkipOnboarding: Boolean = false,
 )
